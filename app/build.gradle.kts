e4f65489a211cf362359901dc35cf5e9acdf9191
kost <kost.t.human@gmail.com>
2026-09-20 10:47:00 +0300

    Версия 0.14.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "ru.kost.ruvoice"
         minSdk = 23
         targetSdk = 35
-        versionCode = 1405
-        versionName = "0.14.5"
+        versionCode = 1406
+        versionName = "0.14.6"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ndk { abiFilters += listOf("arm64-v8a") }
     }
